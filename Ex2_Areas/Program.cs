c4906656d60871d601712f688fe358118da6c9eb
Carol <dleandrosilva@hotmail.com>
2022-07-29 22:51:46 -0300

    Projeto Polimorfismo

---
+++ Ex2_Areas/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ex2_Areas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Ex2_Areas/src/Quadrado.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ex2_Areas.src
+{
+    public class Quadrado
+    {
+        
+    }
+}
